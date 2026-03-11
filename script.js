5929771396cf581b494d96a2d37493bc6d5b9370
candejt <139970803+candejt@users.noreply.github.com>
2026-03-11 18:59:41 +0100

    Revisit rock paper scissors

--- script.js
+++ script.js
@@ -1,79 +1,11 @@
-function getComputerChoice (){
-    let computerChoice = Math.floor(Math.random()*3)
-    if (computerChoice===0){
-        return "rock"
-    }else if (computerChoice===1){
-        return "paper"
-    }else{
-        return "scissors"
-    }
-}
-function getHumanChoice (){
-    let humanChoice = prompt("Play: Rock, Paper, Scissors");
-    return humanChoice
-}
-function playGame (){
 
-    let humanScore = 0
-    let computerScore = 0
 
-   /*simplificar funcion*/
-    function playRound (human,computer){
-        human=human.toLowerCase()
-        if (human === "paper"&& computer==="rock"){
-            humanScore++
-            console.log ("You win! Paper beats rock")
-            console.log ("Score -> HUMAN: " + humanScore + " -Computer: " + computerScore)
-        } else if (human==="paper" && computer=="scissors"){
-            computerScore++
-            console.log ("You lose! Scissors beats paper")
-            console.log ("Score -> HUMAN: " + humanScore + " -Computer: " + computerScore)
-        }else if (human==="paper" && computer==="paper"){
-            console.log("Tie! Play again!")
-            console.log ("Score -> HUMAN: " + humanScore + " -Computer: " + computerScore)
-        } else if (human==="rock" && computer=="paper"){
-            computerScore++
-            console.log ("You lose! Paper beats rock")
-            console.log ("Score -> HUMAN: " + humanScore + " -Computer: " + computerScore)
-        }else if (human==="rock" && computer==="scissors"){
-            humanScore++
-            console.log("You win!Rock beats scissors")
-            console.log ("Score -> HUMAN: " + humanScore + " -Computer: " + computerScore)
-        }else if (human==="rock" && computer==="rock"){
-            console.log("Tie! Play again!")
-            console.log ("Score -> HUMAN: " + humanScore + " -Computer: " + computerScore)
-        } else if (human==="scissors" && computer=="paper"){
-            humanScore++
-            console.log ("You win! Scissors beats paper")
-            console.log ("Score -> HUMAN: " + humanScore + " -Computer: " + computerScore)
-        }else if (human==="scissors" && computer==="rock"){
-            computerScore++
-            console.log("You lose!Rock beats scissors")
-            console.log ("Score -> HUMAN: " + humanScore + " -Computer: " + computerScore)
-        }else {
-            console.log("Tie! Play again!")
-            console.log ("Score -> HUMAN: " + humanScore + " -Computer: " + computerScore)
-        }
-    }
-    
-    for (let i=0; i<5; i++){
-        playRound(getHumanChoice(),getComputerChoice())
-    }
-    
-    console.log ("THE END")
-    console.log ("Final Score -> Human: "+humanScore+ "-Computer: "+computerScore)
-    if (humanScore>computerScore){
-        console.log("You are the WINNER!")
-    }else if (humanScore<computerScore){
-        console.log("Better luck next time LOOSER")
-    }
-    else{
-        console.log("It's a TIE! do you want to try again?")
-    }
-}
+let humanScore = 0
+let computerScore = 0
 
-playGame()
-
+const div=document.createElement("div")
+div.textContent="Click to Play"
+document.body.appendChild(div)
 
 const btnRock=document.createElement("button")
 const btnPaper=document.createElement("button")
@@ -87,4 +19,73 @@ const body = document.querySelector("body")
 body.appendChild(btnRock)
 body.appendChild(btnPaper)
 body.appendChild(btnScissors)
+
+function getComputerChoice (){
+    let computerChoice = Math.floor(Math.random()*3)
+    if (computerChoice===0){
+        return "rock"
+    }else if (computerChoice===1){
+        return "paper"
+    }else{
+        return "scissors"
+    }
+}
+
+function playRound (human,computer){
+    human=human.toLowerCase()
+    if (human === "paper"&& computer==="rock"){
+        humanScore++
+        console.log ("You win! Paper beats rock")
+        console.log ("Score -> HUMAN: " + humanScore + " -Computer: " + computerScore)
+     }else if (human==="paper" && computer=="scissors"){
+        computerScore++
+        console.log ("You lose! Scissors beats paper")
+        console.log ("Score -> HUMAN: " + humanScore + " -Computer: " + computerScore)
+    }else if (human==="paper" && computer==="paper"){
+        console.log("Tie! Play again!")
+        console.log ("Score -> HUMAN: " + humanScore + " -Computer: " + computerScore)
+    } else if (human==="rock" && computer=="paper"){
+        computerScore++
+        console.log ("You lose! Paper beats rock")
+        console.log ("Score -> HUMAN: " + humanScore + " -Computer: " + computerScore)
+    }else if (human==="rock" && computer==="scissors"){
+        humanScore++
+        console.log("You win!Rock beats scissors")
+        console.log ("Score -> HUMAN: " + humanScore + " -Computer: " + computerScore)
+    }else if (human==="rock" && computer==="rock"){
+        console.log("Tie! Play again!")
+        console.log ("Score -> HUMAN: " + humanScore + " -Computer: " + computerScore)
+    } else if (human==="scissors" && computer=="paper"){
+        humanScore++
+        console.log ("You win! Scissors beats paper")
+        console.log ("Score -> HUMAN: " + humanScore + " -Computer: " + computerScore)
+    }else if (human==="scissors" && computer==="rock"){
+        computerScore++
+        console.log("You lose!Rock beats scissors")
+        console.log ("Score -> HUMAN: " + humanScore + " -Computer: " + computerScore)
+    }else {
+        console.log("Tie! Play again!")
+        console.log ("Score -> HUMAN: " + humanScore + " -Computer: " + computerScore)
+    }
+
+    div.textContent="Score -> HUMAN: " + humanScore + " -Computer: " + computerScore
+    if (humanScore===5){
+        div.textContent="You are the WINNER!"
+        return
+       
+    }else if(computerScore===5){
+        div.textContent="Computer wins human"
+        return
+}}
+    
+
+btnRock.addEventListener("click", ()=>{
+    playRound("rock", getComputerChoice())
+})
+btnPaper.addEventListener("click", ()=>{
+    playRound("paper", getComputerChoice())
+})
+btnScissors.addEventListener("click", ()=>{
+    playRound("scissors", getComputerChoice())
+})
 
